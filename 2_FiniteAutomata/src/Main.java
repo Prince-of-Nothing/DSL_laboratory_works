e924eee91a8c92574d8a197102b9a43491fdd156
Pleșu Dinu <plesudinu+waitlist@proton.me>
2026-03-09 13:26:44 +0200

    Variant 20 completion

--- 2_FiniteAutomata/src/Main.java
+++ 2_FiniteAutomata/src/Main.java
@@ -1,14 +1,22 @@
 import java.util.*;
-
+//Variant 20
 public class Main {
     public static void main(String[] args) {
+        //Q
         Set<String> states = new HashSet<>(Arrays.asList("q0", "q1", "q2", "q3"));
+        //∑
         Set<Character> alphabet = new HashSet<>(Arrays.asList('a', 'b','c'));
 
         Map<String, Map<Character, Set<String>>> transitions = new HashMap<>();
 
-        transitions.put("q0", Map.of('a', Set.of("q0")));
+        transitions.put("q0", Map.of('a', Set.of("q0", "q1")));
+        transitions.put("q1", Map.of('b', Set.of("q2")));
+        transitions.put("q2",Map.of('a',Set.of("q2"),'c',Set.of("q3")));
+        transitions.put("q3", Map.of('c', Set.of("q3")));
+        
+        //S
         String startState = "q0";
+        //F
         Set<String> finalStates = Set.of("q3");
 
         FiniteAutomaton fa = new FiniteAutomaton(states, alphabet, transitions, startState, finalStates);
